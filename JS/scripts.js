697a0c35ce300bb0482313bef6b5ef3b68162772
Christine Fruehling and Heather McCarthy <newwavehealing@gmail.com>
2016-02-17 18:59:14 -0800

    Update to add new clickable classes and changed Term and Definition ids to classes.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#Definition").toggle();
-    $("#Term").toggle();
+    $(".Definition").toggle();
+    $(".Term").toggle();
+  });
+  $(".clickable2").click(function() {
+    $(".Definition2").toggle();
+    $(".Term2").toggle();
   });
 });
